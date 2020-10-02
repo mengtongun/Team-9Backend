26407382ccb7d1badf5a6d635965814debbdffed
Mengtongun <unmengtong@gmail.com>
2020-10-02 15:17:15 +0700

    Add search to every category

--- client/service/Service.js
+++ client/service/Service.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 const url = "http://localhost:5000/store/category";
+// const urlSearch = ""
 
 class Service{
     getCategory(category_name){         //Pass the collection name in the parameter to fetch all the store
         return axios.get(`${url}/${category_name}`);
     
     }
-    searchStore(text){
-        
+    searchStore(category_name,store_name){
+        return axios.get(`${url}/${category_name}/search/${store_name}`)
     }
 };
 export default Service;
